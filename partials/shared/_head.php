10c0de3ad05565ca2b0d6659b38ae4383425e6a0
Jeffrey Tierney <jeff@new-host.home>
2010-04-10 10:53:31 -0400

    added is_mobile param to environment, and started prepping head for mobile

--- partials/shared/_head.php
+++ partials/shared/_head.php
@@ -1,3 +1,5 @@
+<meta name="viewport" content="width=device-width,minimum-scale=1.0, maximum-scale=1.0" />
+
 <link href="<?php echo SC::root(); ?>css/sc.css" rel="stylesheet" type="text/css" />
 
 <script type="text/javascript" src="<?php echo SC::root(); ?>js/jquery-1.4.2.min.js"></script>
