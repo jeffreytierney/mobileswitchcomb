4a2a897e62b6260e072751fd7b836a1ddf54d654
Jeff Tierney <jeff@tierney.me>
2011-06-22 09:45:02 -0400

    start js fixing on mobile

--- js/sc.mobile.js
+++ js/sc.mobile.js
@@ -1,3 +1,5 @@
+/*
 SC.util.registerGlobalHook(SC.ReplyCreateForm, "sc_replyform_show", function() {
   setTimeout(function() {window.scrollTo(0, 9999999);}, 250);
 });
+*/
--- partials/shared/_head.php
+++ partials/shared/_head.php
@@ -1,16 +1,3 @@
 <meta name="viewport" content="width=device-width,minimum-scale=1.0, maximum-scale=1.0" />
 
 <link href="<?php echo SC::cssPath("sc"); ?>" rel="stylesheet" type="text/css" />
-
-<script type="text/javascript" src="<?php echo SC::jsPath("jquery-1.4.2.min"); ?>"></script>
-<script type="text/javascript" src="<?php echo SC::jsPath("jquery.form"); ?>"></script>
-<script type="text/javascript" src="<?php echo SC::jsPath("json2"); ?>"></script>
-<script type="text/javascript" src="<?php echo SC::jsPath("sc"); ?>"></script>
-<script type="text/javascript" src="<?php echo SC::jsPath("sc.board"); ?>"></script>
-<script type="text/javascript" src="<?php echo SC::jsPath("sc.mobile"); ?>"></script>
-
-<script type="text/javascript">
-
-  SC.data.current_user = <?php echo $current_user ? $current_user->jsonify() : "null"; ?>;
-
-</script>
